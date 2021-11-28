ef623f5927adf5337501bb1ea2368c118a93ff58
Sameh-farouk <samehabouelsaad@gmail.com>
2021-11-28 14:36:31 +0200

    support overriding default password

--- docker/scripts/caprover_leader.sh
+++ docker/scripts/caprover_leader.sh
@@ -18,7 +18,8 @@ if [[ ! -z "${CAPROVER_ROOT_DOMAIN}" ]]; then
 
 fi
 
-while ! docker run -p 80:80 -p 443:443 -p 3000:3000 -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain $CAPTAIN_IMAGE:$CAPTAIN_IMAGE_VERSION; do
+[ -z "$DEFAULT_PASSWORD" ] && DEFAULT_PASSWORD="captain42"
+while ! docker run -e DEFAULT_PASSWORD='${DEFAULT_PASSWORD}' -p 80:80 -p 443:443 -p 3000:3000 -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain $CAPTAIN_IMAGE:$CAPTAIN_IMAGE_VERSION; do
     sleep 2
 done
 
