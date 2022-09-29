5bcd1f13b95bd1cfcddba68e138ac4ff76d48bce
rawdaGastan <rody.rawda.rr@gmail.com>
2022-09-29 15:09:27 +0200

    allow env CAPTAIN_IS_DEBUG

--- docker/scripts/caprover_leader.sh
+++ docker/scripts/caprover_leader.sh
@@ -19,7 +19,7 @@ if [[ ! -z "${CAPROVER_ROOT_DOMAIN}" ]]; then
 fi
 
 [ -z "$DEFAULT_PASSWORD" ] && DEFAULT_PASSWORD="captain42"
-while ! docker run -e DEFAULT_PASSWORD="$DEFAULT_PASSWORD" -p 80:80 -p 443:443 -p 3000:3000 -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain $CAPTAIN_IMAGE:$CAPTAIN_IMAGE_VERSION; do
+while ! docker run -e DEFAULT_PASSWORD="$DEFAULT_PASSWORD" -e CAPTAIN_IS_DEBUG="$CAPTAIN_IS_DEBUG" -p 80:80 -p 443:443 -p 3000:3000 -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain $CAPTAIN_IMAGE:$CAPTAIN_IMAGE_VERSION; do
     sleep 2
 done
 
@@ -27,4 +27,5 @@ echo "==================================="
 echo " **** Installation is done! *****  "
 [[ ! -z "${CAPROVER_ROOT_DOMAIN}" ]] && echo "CapRover is available at http://captain.${CAPROVER_ROOT_DOMAIN}" || echo "CapRover is available at http://{public-ip-address}:3000"
 echo "Default password is: captain42"
+echo "CapRover CAPTAIN IS DEBUG: ${CAPTAIN_IS_DEBUG}"
 echo "==================================="
