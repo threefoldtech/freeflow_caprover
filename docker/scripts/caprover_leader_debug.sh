d92babe45fe9f31daf166aa6a50c2ed3604081ff
rawdaGastan <rody.rawda.rr@gmail.com>
2022-10-04 14:01:32 +0200

    add debug

--- docker/scripts/caprover_leader_debug.sh
+++ docker/scripts/caprover_leader_debug.sh
@@ -1,24 +1,3 @@
-CAPTAIN_IMAGE=${CAPTAIN_IMAGE:-rawdgastan/caprover}
-CAPTAIN_IMAGE_VERSION=${CAPTAIN_IMAGE_VERSION:-captain-debug}
-
-if [[ ! -z "${CAPROVER_ROOT_DOMAIN}" ]]; then
-    echo "CapRover Root Domain: ${CAPROVER_ROOT_DOMAIN}"
-    mkdir -p /captain/data
-    echo "{
-        \"namespace\": \"captain\",
-        \"customDomain\": \"${CAPROVER_ROOT_DOMAIN}\"
-    }" >/captain/data/config-captain.json
-    cat /captain/data/config-captain.json
-    echo "{
-        \"publishedNameOnDockerHub\": \"$CAPTAIN_IMAGE\",
-        \"version\": \"$CAPTAIN_IMAGE_VERSION\",
-        \"skipVerifyingDomains\":\"true\"
-    }" >/captain/data/config-override.json
-    echo "CapRover will be available at http://captain.${CAPROVER_ROOT_DOMAIN} after installation"
-
-fi
-
-docker login -u rawdagastan -p Xx11221122
 
 if ! [ $(id -u) = 0 ]; then
    echo "Must run as sudo or root"
